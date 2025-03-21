4f32d0fa7d68e3f1352251ec024f72ca2675aa0d
frovaaa <frovad@usi.ch>
2025-03-21 19:21:24 +0100

    Trying to add joystick

--- src/components/JoystickControl.tsx
+++ src/components/JoystickControl.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect } from 'react';
 import ROSLIB, { Ros } from 'roslib';
-import nipplejs from 'nipplejs';
 
 interface JoystickControlProps {
     ros: Ros | null;
@@ -10,46 +9,58 @@ interface JoystickControlProps {
 
 export default function JoystickControl({ ros }: JoystickControlProps) {
     useEffect(() => {
+        let manager: any;
+
         if (ros) {
             const joystickContainer = document.getElementById('joystick');
             if (joystickContainer) {
-                const manager = nipplejs.create({
-                    zone: joystickContainer,
-                    mode: 'static',
-                    position: { left: '50%', bottom: '50px' },
-                    color: 'blue',
-                });
-
-                const cmdVelTopic = new ROSLIB.Topic({
-                    ros: ros,
-                    name: '/cmd_vel',
-                    messageType: 'geometry_msgs/Twist',
-                });
-
-                manager.on('move', (evt, data) => {
-                    if (data && data.vector) {
-                        const linearX = data.vector.y; // Forward/backward
-                        const angularZ = data.vector.x; // Left/right
-
-                        const twist = new ROSLIB.Message({
-                            linear: { x: linearX, y: 0, z: 0 },
-                            angular: { x: 0, y: 0, z: angularZ },
-                        });
-
-                        cmdVelTopic.publish(twist);
-                    }
-                });
-
-                manager.on('end', () => {
-                    // Stop the robot when joystick is released
-                    const stopTwist = new ROSLIB.Message({
-                        linear: { x: 0, y: 0, z: 0 },
-                        angular: { x: 0, y: 0, z: 0 },
+                // Dynamically import nipplejs
+                import('nipplejs').then((nipplejs) => {
+                    manager = nipplejs.create({
+                        zone: joystickContainer,
+                        mode: 'static',
+                        position: { left: '50%', bottom: '50px' },
+                        color: 'blue',
                     });
-                    cmdVelTopic.publish(stopTwist);
+
+                    const cmdVelTopic = new ROSLIB.Topic({
+                        ros: ros,
+                        name: '/cmd_vel',
+                        messageType: 'geometry_msgs/Twist',
+                    });
+
+                    manager.on('move', (evt: any, data: any) => {
+                        if (data && data.vector) {
+                            const linearX = data.vector.y; // Forward/backward
+                            const angularZ = data.vector.x; // Left/right
+
+                            const twist = new ROSLIB.Message({
+                                linear: { x: linearX, y: 0, z: 0 },
+                                angular: { x: 0, y: 0, z: angularZ },
+                            });
+
+                            cmdVelTopic.publish(twist);
+                        }
+                    });
+
+                    manager.on('end', () => {
+                        // Stop the robot when joystick is released
+                        const stopTwist = new ROSLIB.Message({
+                            linear: { x: 0, y: 0, z: 0 },
+                            angular: { x: 0, y: 0, z: 0 },
+                        });
+                        cmdVelTopic.publish(stopTwist);
+                    });
                 });
             }
         }
+
+        // Cleanup on unmount
+        return () => {
+            if (manager) {
+                manager.destroy();
+            }
+        };
     }, [ros]);
 
     return <div id="joystick" style={{ width: '200px', height: '200px', margin: '0 auto' }}></div>;
